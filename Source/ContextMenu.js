4a02c8b71623aad7e770fbede70b4817f79b1699
David Walsh <davidwalsh83@gmail.com>
2009-11-29 13:05:21 -0600

    Updating to 1.1

--- Source/ContextMenu.js
+++ Source/ContextMenu.js
@@ -1,11 +1,20 @@
 /*
-Script: ContextMenu
+---
+description:     ContextMenu
 
-License: MIT-style license.
+authors:
+  - David Walsh (http://davidwalsh.name)
 
-Copyright: Copyright (c) 2007-2009 [David Walsh](http://davidwalsh.name/).
+license:
+  - MIT-style license
 
-Author: David Walsh (http://davidwalsh.name)
+requires:
+  core/1.2.1:   '*'
+  more/1.2.1:   'Fx.*'
+
+provides:
+  - ContextMenu
+...
 */
 var ContextMenu = new Class({
 
